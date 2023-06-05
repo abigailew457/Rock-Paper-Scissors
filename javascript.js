e5ab97817c7ad813a779fbcbd5cf286720a5cdf3
abigailew457 <abigailew457@gmail.com>
2023-06-05 22:14:32 +0900

    Randomize C.Choice

--- javascript.js
+++ javascript.js
@@ -1,5 +1,15 @@
 console.log('Hello World!')
 function getComputerChoice() {
-    
-}
+    let compChoice = ['Rock', 'Paper', 'Scissors'];
+    let randomChoice = Math.floor(Math.random() * compChoice.length) + 1;
 
+    if (randomChoice === 1) {
+        return 'Rock';
+    }
+    if (randomChoice === 2) {
+        return 'Paper';
+    }
+    if (randomChoice === 3) {
+        return 'Scissors';
+    }
+}
