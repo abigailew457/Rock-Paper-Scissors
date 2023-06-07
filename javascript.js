4f57f37f6a67c07bb176749468eb47deee5888d6
abigailew457 <abigailew457@gmail.com>
2023-06-07 20:25:59 +0900

    Add notes to C.Choice

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 console.log('Hello World!')
 function getComputerChoice() {
-    let compChoice = ['Rock', 'Paper', 'Scissors'];
-    let randomChoice = Math.floor(Math.random() * compChoice.length) + 1;
+    let compChoice = ['Rock', 'Paper', 'Scissors']; // Establishes computer choice options
+    let randomChoice = Math.floor(Math.random() * compChoice.length) + 1; // Randomizing the chompChoice by creating a random number, multiplying it by the length of the choice, rounding it down, and adding it by 1
 
     if (randomChoice === 1) {
         return 'Rock';
@@ -11,6 +11,6 @@ function getComputerChoice() {
     }
     if (randomChoice === 3) {
         return 'Scissors';
-    }
+    } // Applying a clear string to the resutling numbers
 }
 console.log(getComputerChoice())
