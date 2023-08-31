8be776043cd8fb860b544326a686aa4ccce873e5
abigailew457 <abigailew457@gmail.com>
2023-08-31 14:36:33 +0900

    Add styles and text to buttons

--- javascript.js
+++ javascript.js
@@ -50,15 +50,21 @@ const container = document.querySelector('#container');
 
 const rock = document.createElement('button');
 rock.setAttribute('id', 'rock');
+rock.setAttribute('style', 'height: 40px; width: 100px;');
+rock.textContent = 'Rock';
 console.log(rock);
 container.appendChild(rock); 
 
 const paper = document.createElement('button');
 paper.setAttribute('id', 'paper');
+paper.setAttribute('style', 'height: 40px; width: 100px;');
+paper.textContent = 'Paper';
 console.log(paper);
 container.appendChild(paper); 
 
 const scissors = document.createElement('button');
 scissors.setAttribute('id', 'scissors');
+scissors.setAttribute('style', 'height: 40px; width: 100px;');
+scissors.textContent = 'Scissors';
 console.log(scissors);
 container.appendChild(scissors); 
